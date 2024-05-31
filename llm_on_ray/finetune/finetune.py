e363c0933ecde2af4e6f0e7fa24e8ad4d835b81f
minmingzhu <minming.zhu@intel.com>
2024-05-31 16:11:00 +0800

    update

--- llm_on_ray/finetune/finetune.py
+++ llm_on_ray/finetune/finetune.py
@@ -134,6 +134,7 @@ def convert_dtype(dtype: str) -> Optional[torch.dtype]:
     }
     return supported_dtypes[dtype]
 
+
 def load_tokenizer(config: Dict):
     if config["General"].get("tokenizer_name") is not None:
         tokenizer_name = config["General"].get("tokenizer_name")
@@ -270,6 +271,7 @@ def tokenize_dataset(config: Dict, tokenizer, dataset):
 
     return tokenized_dataset
 
+
 def prepare_data_collator(config: Dict, tokenizer):
     return transformers.DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=False, return_tensors="pt", pad_to_multiple_of=8
@@ -356,6 +358,7 @@ def get_trainer(config: Dict, model, tokenizer, tokenized_datasets, data_collato
             )
         return training_args, trainer
 
+
 def train_func(config: Dict[str, Any]):
     os.chdir(config["cwd"])
 
@@ -379,6 +382,7 @@ def train_func(config: Dict[str, Any]):
     trainer.train(resume_from_checkpoint=training_args.resume_from_checkpoint)
     trainer.save_model()
     common.logger.info("train finish")
+
 
 def get_finetune_config():
     parser = argparse.ArgumentParser(
